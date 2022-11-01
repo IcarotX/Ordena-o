54cbd4e36d52a7ee1b65dc8546e74f51ab997ca2
IcarotX <117233820+IcarotX@users.noreply.github.com>
2022-11-01 20:06:58 -0300

    ordena.java

---
+++ main.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    
+    int[] vetor = new int[6];
+    Random ds = new Random();
+
+    ds.setSeed(65596);
+    
+    for(int i=0;i<=5;i++){
+     vetor[i] = ds.nextInt(999);
+     }
+    ordena.impressao(vetor);
+
+    System.out.println();
+
+    long inicio = System.nanoTime();
+ // Método Ordenação 
+    ordena.Buble(vetor);
+    long fim = System.nanoTime();
+    long intervalo = fim - inicio;
+    ordena.impressao(vetor);
+    ordena.status(inicio, fim, intervalo);
+
+  }
+}
